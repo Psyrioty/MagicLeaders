5e2a1bb28fd287221a12b2afad32670180a7db1b
psyrioty <psyrioty@hotmail.com>
2026-06-17 00:21:21 +0300

    Initial commit

---
+++ src/main/java/org/psyrioty/magicLeaders/MagicLeaders.java
@@ -0,0 +1,16 @@
+package org.psyrioty.magicLeaders;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MagicLeaders extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
